4e34518e4a2f6cce41ab9ed46299f2900a7ca9ff
DonovanFranco <161343179+DonovanFranco@users.noreply.github.com>
2024-04-14 08:47:31 -0600

    Create main.js
    Archivos iniciales

---
+++ js/main.js
@@ -0,0 +1,54 @@
+const canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+
+//Obtiene las dimensiones de la pantalla actual
+const window_height=window.innerHeight;
+const window_width=window.innerWidth;
+
+//El canvas tiene las mismas dimensiones que la pantalla
+canvas.height=window_height;
+canvas.width=window_width;
+
+canvas.style.backgroundColor='#ff8';
+
+class Circle{
+    constructor(x, y, radius, color, text){
+        this.posX= x;
+        this.posY= y;
+        this.radius= radius;
+        this.color= color;
+        this.text= text; //Texto
+    }
+    draw(context){
+        context.beginPath();
+        context.strokeStyle=this.color;
+        context.textAlign = "center";
+        context.textBaseline = "middle";
+        context.font = "20px Arial";
+        context.fillText(this.text, this.posX, this.posY);
+        context.lineWidth = 3;
+        context.arc(this.posX, this.posY , this.radius, 0, Math.PI*2, false);
+        context.stroke();
+        context.closePath();
+    }
+}
+
+let arrayCircle=[];
+
+for (let i = 0; i < 10; i++) {
+    let randomX = Math.random() * window_width;
+    let randomY = Math.random() * window_height;
+
+    let randomRadius = Math.floor(Math.random()*100+30);
+    let miCirculo = new Circle(randomX,randomY,randomRadius,'blue',i+1);
+
+    //Agrega el objeto al array
+    arrayCircle.push(miCirculo);
+    arrayCircle[i].draw(ctx);
+}
+
+/* let miCirculo = new Circle(100,100,50,'blue','Tec');
+miCirculo.draw(ctx);
+
+miCirculo2 = new Circle(270, 270,50, 'black', 'Pachuca');
+miCirculo2.draw(ctx); */
